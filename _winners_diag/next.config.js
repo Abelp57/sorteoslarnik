93fd6c8e34c2a8f2e7c8c23eabf5486e14390471
server-fix <server@local>
2025-10-15 03:41:24 +0000

    chore: remove _winners_diag from production build

--- _winners_diag/next.config.js
+++
@@ -1,4 +0,0 @@
-/** @type {import('next').NextConfig, images: { remotePatterns: [ { protocol: "https", hostname: "i.postimg.cc" }, { protocol: "https", hostname: "postimg.cc" } ] } } */
-const nextConfig = { reactStrictMode: true , images: { remotePatterns: [ { protocol: "https", hostname: "i.postimg.cc" }, { protocol: "https", hostname: "postimg.cc" } ] } };
-module.exports = nextConfig;
-
